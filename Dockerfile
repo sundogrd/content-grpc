fd3c1dc75c6541ab3fabdbe6d79135f039fe726f
breakinferno <1972952841@qq.com>
2019-07-29 09:35:18 +0800

    chore: 修改配置文件

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,11 @@ COPY . $GOPATH/src/github.com/sundogrd/content-grpc
 WORKDIR $GOPATH/src/github.com/sundogrd/content-grpc
 
 ENV GO111MODULE=on
-
+ARG DB_USER
+ARG DB_PWD
 RUN go build .
 
-RUN "./devops/build_docker.sh"
+RUN "./devops/build_docker.sh" $DB_USER $DB_PWD
 
 EXPOSE 25555
   
--- devops/build_docker.sh
+++ devops/build_docker.sh
@@ -2,4 +2,8 @@
 
 cp ./config/service.template.config.json ./config/service.config.json
 
+sed -i "s/Your Username/$1/g" "./config/service.config.json"
+sed -i "s/Your Password/$2/g" "./config/service.config.json"
+
+
 ip -4 route list match 0/0 | awk '{print $3 " host.docker.internal"}' >> /etc/hosts
